179ebcb3c84479fbec0c618b8aeee1653836318d
DeadNewMouseG <camonba@hotmail.com>
2024-12-05 10:13:21 -0500

    feat: menu

--- App_Metodos/src/app/components/home/home.component.ts
+++ App_Metodos/src/app/components/home/home.component.ts
@@ -1,9 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, InputTextModule, ButtonModule, PasswordModule, CommonModule, HttpClientModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
